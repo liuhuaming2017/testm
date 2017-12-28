5c9f97a7975558bbeaa561fc6dee851fab7ddcac
paas <paas@cse-codegen-backend-3345176999-xwl50>
2017-12-28 03:35:09 +0000

    Auto pushed by ServiceStage!

---
+++ src/main/java/com/service/cptstest/controller/CptstestDelegate.java
@@ -0,0 +1,14 @@
+package com.service.cptstest.controller;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class CptstestDelegate {
+
+    public String helloworld(String name){
+
+        // Do Some Magic Here!
+        return name;
+    }
+}
